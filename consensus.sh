d9ece884d61fcb1d645a29e4fe7802064ee9e1cc
Alexis-Marion <80922475+Corvus-Alex@users.noreply.github.com>
2022-01-14 09:54:12 +0100

    Update consensus.sh

--- consensus.sh
+++ consensus.sh
@@ -1,15 +1,23 @@
-for a in output/*/; do
-for c in output/*; do
+mkdir out_align
+for a in output/*; do
     for b in ${a}/*; do
-        c1=${c##*/}
+        a1=${a##*/}
+        a2="out_align/${a1}"
         b1=${b##*/}
-        realname="output/${c1}_${b1}.fasta"
-        realname2="output/${c1}_${b1}_aligned.fasta"
-        realname3="output/${c1}_${b1}_aligned_consensus.fst"
+        a3=${a2##*/}
+        mkdir ${a2}
+        realname="${a2}/${a1}_${b1}.fasta"
+        realname2="${a2}/${a1}_${b1}_aligned.fasta"
+        realname3="${a2}/${a1}_${b1}_aligned_consensus.fst"
         cat ${b}/* >> ${realname}
         muscle -in ${realname} -out ${realname2}
-        em_cons ${realname2} ${realname3} -name ${b1}
+        em_cons ${realname2} ${realname3} -name ${a3}
+        rm ${a2}/*.fasta
     done
+    a4="${a2}/${a1}_aligned_consensus_multi.faa"
+    cat ${a2}/*.fst >> ${a4}
+    a5="${a2}/${a1}_aligned_consensus_multi_cleaned.fasta"
+    union -filter ${a4} >> ${a5}
+    rm ${a2}/*fst
+    rm ${a2}/*faa
 done
-done
-rm output/*.fasta
