b345a9d830d245139e0c1ffe0041fe24491b593c
Corvus-Alex <80922475+Corvus-Alex@users.noreply.github.com>
2022-02-17 14:46:49 +0100

    Update tree_maker.sh

--- tree_maker.sh
+++ tree_maker.sh
@@ -1,3 +1,3 @@
 seqkit concat last/*.fst >> last/my_aligned_sequences.fst
-iqtree -s last/my_aligned_sequences.fst -st DNA -o Chimaera_monstrosa -bb 1000 -spp partition_file2.txt -m MFP+MERGE -safe
+iqtree2 -s last/my_aligned_sequences.fst -st DNA -o Chimaera_monstrosa -bb 1000 -spp partition_file2.txt -m MFP+MERGE -safe
 
